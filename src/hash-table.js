755a4976e54bd5b49b657d838f4e17ee0b7d9520
atiffany <abby.tiffany@gmail.com>
2018-02-23 15:04:11 -0800

    hash in progress

--- src/hash-table.js
+++ src/hash-table.js
@@ -14,12 +14,18 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
+    const location = getIndexBelowMax(key.toString(), this.limit);
+    const keyString = key + '';
+    const bucket = {...this.storage.get(location), keyString: value };
+    this.storage.get(location) ? this.storage.set(location, bucket) : this.storage.set(location, value);
     
   }
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
+    const location = getIndexBelowMax(key.toString(), this.limit);
+    if(this.storage.get(location)) this.storage.set(location, undefined);
     
   }
   
@@ -27,7 +33,9 @@ class HashTable {
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
-    
+    const location = getIndexBelowMax(key + '', this.limit);
+    const value = this.storage.get(location);
+    return value[key];
   }
 }
 
