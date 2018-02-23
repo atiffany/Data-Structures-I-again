93457087eb6b89c32eb39f5d8e73b0adedd128de
atiffany <abby.tiffany@gmail.com>
2018-02-23 09:34:05 -0800

    completed Queue with 2 Stacks

--- src/queue.js
+++ src/queue.js
@@ -4,8 +4,30 @@
   3. Add an `enqueue` method that accepts an item as input and adds it to the storage structure
   4. Add a `dequeue` method that removes the item in the queue that was added earliest
 */
-class Queue {
+const Stack = require('./stack');
 
+class Queue {
+  constructor() {
+    this.storage1 = new Stack();
+    this.storage2 = new Stack();
+  }
+
+  get size() {
+    return this.storage1 + this.storage2;
+  }
+
+  enqueue(item) {
+    this.storage1.push(item);
+  }
+
+  dequeue() {
+    if (this.storage2.size < 1) {
+      while (this.storage1.size > 0) {
+        this.storage2.push(this.storage1.pop());
+      }
+    }
+    return this.size() ? this.storage2.pop() : null;
+  }
 }
 
 module.exports = Queue;
